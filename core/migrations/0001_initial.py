53ca7ea4fa756b431445b42ab31170372503ef3c
clonehugger <andrea.puente@udem.edu>
2017-05-13 16:41:07 -0500

    model capitalization

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
             name='order',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('client', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='core.client')),
+                ('client', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='core.models.Client')),
             ],
         ),
         migrations.CreateModel(
--- core/models.py
+++ core/models.py
@@ -4,31 +4,31 @@ from django.db import models
 from djmoney.models.fields import MoneyField
 
 
-class client(models.Model):
+class Client(models.Model):
     """client info"""
     name = models.CharField(max_length=100)
     mail = models.EmailField()
 
-class package(models.Model):
+class Package(models.Model):
     """packages: aditional options for templates"""
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=255)
     price = MoneyField(max_digits=10, decimal_places=2, default_currency='MXN')
 
 
-class template(models.Model):
+class Template(models.Model):
     """template: """
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     photo_cover = models.FileField(upload_to='covers')
     url = models.URLField()
     price = MoneyField(max_digits=10, decimal_places=2, default_currency='MXN')
-    packages = models.ManyToManyField(package)
+    packages = models.ManyToManyField(Package)
 
-class order(models.Model):
+class Order(models.Model):
     """a client order"""
-    template = models.OneToOneField(template)
-    client = models.OneToOneField(client)
+    template = models.OneToOneField(Template)
+    client = models.OneToOneField(Client)
 
 
 
