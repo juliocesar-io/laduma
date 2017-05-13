6345bc78168a7299c464aa8d055ce3a065fcd3be
Julio César <julioc255io@gmail.com>
2017-05-13 15:51:27 -0500

    add templates/styles

--- laduma/settings.py
+++ laduma/settings.py
@@ -116,7 +116,14 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+
+STATICFILES_DIRS = (
+    os.path.join('laduma/static'),
+)
+
 
 STATIC_URL = '/static/'
